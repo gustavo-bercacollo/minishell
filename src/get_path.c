cc8ac0cfd5eb70232cd59b2aac52630ea304a61f
kaio lima <kylelima850@hotmail.com>
2025-11-17 14:46:14 -0300

    feat: implement executable path resolver using PATH from envp

---
+++ src/get_path.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/08 17:18:06 by klima-do          #+#    #+#             */
+/*   Updated: 2025/11/17 14:40:59 by klima-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**ft_free_split(char **arr_ptr)
+{
+	int	index;
+
+	index = 0;
+	if (!arr_ptr)
+		return (NULL);
+	while (arr_ptr[index])
+	{
+		free(arr_ptr[index]);
+		index++;
+	}
+	free(arr_ptr);
+	return (NULL);
+}
+
+char	*find_env_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	if (!envp[i])
+		return (NULL);
+	return (envp[i] + 5);
+}
+
+char	*get_path(char **envp, char *cmd)
+{
+	char	*envp_path;
+	int		j;
+	char	**dirs;
+	char	*path;
+	char	*temp;
+
+	envp_path = find_env_path(envp);
+	dirs = ft_split(envp_path, ':');
+	j = 0;
+	while (dirs[j])
+	{
+		temp = ft_strjoin(dirs[j], "/");
+		path = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(path, X_OK) == 0)
+		{
+			ft_free_split(dirs);
+			return (path);
+		}
+		free(path);
+		j++;
+	}
+	ft_free_split(dirs);
+	return (NULL);
+}
