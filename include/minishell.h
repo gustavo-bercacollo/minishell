fb54ca956adfb33830933eec7106e2ba2c2a10a8
kaio lima <kylelima850@hotmail.com>
2025-11-17 14:43:13 -0300

    feat: add base header with token types and structures

--- include/minishell.h
+++ include/minishell.h
@@ -4,5 +4,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum e_toktype
+{
+	TOK_WORD,
+	TOK_PIPE,
+	TOK_REDIR_IN,
+	TOK_REDIR_OUT,
+	TOK_REDIR_APPEND,
+	TOK_HEREDOC
+}	t_toktype;
+
+typedef struct s_token
+{
+	char			*value;
+	t_toktype		type;
+	struct s_token	*list;
+}	t_token;
+
+
+typedef struct t_shell
+{
+	char	**envp;
+	char	*input;
+	int		last_status;
+	t_token	*tokens;
+}	t_shell;
+
+/* Tokenizer */
+t_token *tokenize(char *input);
+
+/* Parser */
+void    parser(t_shell *ms);
+
+/* Exec */
+void    executor(t_shell *ms);
+
+/* Utils */
+void    free_tokens(t_token *list);
 
 #endif
