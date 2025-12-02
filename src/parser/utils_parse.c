99cad7f5eee9fb60b683b234c37b846c39fdd0b8
Gustavo Bercacollo Vilela <gbercaco@c2r10p2.42sp.org.br>
2025-12-02 17:49:31 -0300

    refactor(utils_parse): creating new helpers functions to simplify the logic

--- src/parser/utils_parse.c
+++ src/parser/utils_parse.c
@@ -6,7 +6,7 @@
 /*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 17:38:23 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/12/02 15:13:35 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/12/02 17:38:35 by gbercaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,38 @@ t_command	*new_command(void)
 	return (cmd);
 }
 
+static void	replace_old_arrays(t_command *cmd, char **new_argv, int *new_quoted)
+{
+	free(cmd->argv);
+	free(cmd->quoted);
+	cmd->argv = new_argv;
+	cmd->quoted = new_quoted;
+}
+
+static void	copy_old_args(t_command *cmd, char **new_argv, int *new_quoted)
+{
+	int	i;
+
+	i = 0;
+	while (i < cmd->argc)
+	{
+		new_argv[i] = cmd->argv[i];
+		new_quoted[i] = cmd->quoted[i];
+		i++;
+	}
+}
+
+static void	add_new_arg_and_set_null(t_command *cmd, t_token *tok, char **new_argv,
+		int *new_quoted)
+{
+	new_argv[cmd->argc] = ft_strdup(tok->value);
+	new_quoted[cmd->argc] = tok->single_quoted;
+	new_argv[cmd->argc + 1] = NULL;
+	new_quoted[cmd->argc + 1] = 0;
+}
+
 void	add_arg(t_command *cmd, t_token *tok)
 {
-	int		i;
 	char	**new_argv;
 	int		*new_quoted;
 
@@ -46,19 +75,8 @@ void	add_arg(t_command *cmd, t_token *tok)
 		free(new_argv);
 		return ;
 	}
-	i = -1;
-	while (++i < cmd->argc)
-	{
-		new_argv[i] = cmd->argv[i];
-		new_quoted[i] = cmd->quoted[i];
-	}
-	new_argv[cmd->argc] = ft_strdup(tok->value);
-	new_quoted[cmd->argc] = tok->single_quoted;
-	new_argv[cmd->argc + 1] = NULL;
-	new_quoted[cmd->argc + 1] = 0;
-	free(cmd->argv);
-	free(cmd->quoted);
-	cmd->argv = new_argv;
-	cmd->quoted = new_quoted;
+	copy_old_args(cmd, new_argv, new_quoted);
+	add_new_arg_and_set_null(cmd, tok, new_argv, new_quoted);
+	replace_old_arrays(cmd, new_argv, new_quoted);
 	cmd->argc++;
 }
