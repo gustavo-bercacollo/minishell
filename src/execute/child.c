9194309dd0dd79e9a4d2c5116e0c67ac1ba59d80
kaio lima <kylelima850@hotmail.com>
2025-12-10 16:27:40 -0300

    fix(exec): ensure heredoc creation relates correctly to fork process

--- src/execute/child.c
+++ src/execute/child.c
@@ -1,49 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   child.c                                            :+:      :+:    :+:   */
+/*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/04 15:45:47 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/12/05 16:42:19 by gbercaco         ###   ########.fr       */
+/*   Created: 2025/11/17 18:53:53 by klima-do          #+#    #+#             */
+/*   Updated: 2025/12/05 17:58:24 by gbercaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exec_child(t_shell *ms, t_command *cmd)
+void	run_child(t_shell *ms, t_command *cmd, int fd_in, int fd[2])
 {
-	char	*path;
+	char *path;
 
+	if (cmd->heredoc == 1)
+	{
+		dup2(cmd->heredoc_fd, STDIN_FILENO);
+		close(cmd->heredoc_fd);
+	}
+	else if (fd_in != 0)
+	{
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
+	}
+	if (cmd->infile)
+		handle_infile(cmd);
+	if (cmd->next)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+	}
+	if (cmd->outfile)
+		handle_outfile(cmd);
 	path = get_path(ms->envp, cmd->argv[0]);
 	if (!path)
 	{
-		printf("minishell: command not found: %s\n", cmd->argv[0]);
+		ft_putstr_fd("minishell: command not found: ", 2);
+		ft_putendl_fd(cmd->argv[0], 2);
 		exit(127);
 	}
 	execve(path, cmd->argv, ms->envp);
 	perror("execve");
 	exit(1);
-}
-
-void	run_child(t_shell *ms, t_command *cmd, int fd_in, int fd[2])
-{
-	set_default_signals_for_child();
-	if (cmd->heredoc)
-		handle_heredoc(cmd);
-	else if (fd_in != 0)
-		handle_pipe_input(fd_in);
-	if (cmd->next)
-		handle_pipe_output(fd);
-	if (cmd->outfile)
-		handle_outfile(cmd);
-	if (cmd->infile)
-		handle_infile(cmd);
-	// if (is_builtin(cmd->argv[0]))
-	// {
-	// 	ms->last_status = execute_builtin(ms, cmd);
-	// 	exit(ms->last_status);
-	// }
-	exec_child(ms, cmd);
 }
