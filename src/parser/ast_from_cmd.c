687a928fc04341d4b64aa1e546057994619a9256
kaio lima <kylelima850@hotmail.com>
2025-12-18 16:01:19 -0300

    feat: add constructor function for command nodes in AST

---
+++ src/parser/ast_from_cmd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast_from_cmd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 15:51:39 by klima-do          #+#    #+#             */
+/*   Updated: 2025/12/18 15:55:38 by klima-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_ast *ast_from_cmd(t_command *cmd)
+{
+	t_ast	*node_cmd;
+
+	node_cmd = malloc(sizeof(t_ast));
+	if (!node_cmd)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	node_cmd->type = NODE_CMD;
+	node_cmd->left = NULL;
+	node_cmd->right = NULL;
+	node_cmd->cmd = cmd;
+	return (node_cmd);
+}
