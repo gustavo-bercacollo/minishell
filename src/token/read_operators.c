ed5e75774aa9a5144bdf0a6334e27ccfd3fcabee
kaio lima <kylelima850@hotmail.com>
2025-12-22 20:55:16 -0300

    refactor: add safe_new_op to handle token allocation failures

--- src/token/read_operators.c
+++ src/token/read_operators.c
@@ -3,41 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   read_operators.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
+/*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 19:26:28 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/12/03 15:42:30 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/12/22 20:07:34 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static t_token	*safe_new_op(char *value, t_toktype type)
+{
+	t_token	*tok;
+
+	tok = new_token(value, type);
+	if (!tok)
+	{
+		free(value);
+		return (NULL);
+	}
+	return (tok);
+}
+
 static t_token	*read_pipe(char **s)
 {
+	char	*value;
+
 	(*s)++;
-	return (new_token(ft_strdup("|"), TOK_PIPE));
+	value = ft_strdup("|");
+	if (!value)
+		return (NULL);
+	return (safe_new_op(value, TOK_PIPE));
 }
 
 static t_token	*read_greater(char **s)
 {
+	char	*value;
+
 	(*s)++;
 	if (**s == '>')
 	{
 		(*s)++;
-		return (new_token(ft_strdup(">>"), TOK_REDIR_APPEND));
+		value = ft_strdup(">>");
+		if (!value)
+			return (NULL);
+		return (safe_new_op(value, TOK_REDIR_APPEND));
 	}
-	return (new_token(ft_strdup(">"), TOK_REDIR_OUT));
+	value = ft_strdup(">");
+	if (!value)
+		return (NULL);
+	return (safe_new_op(value, TOK_REDIR_OUT));
 }
 
 static t_token	*read_less(char **s)
 {
+	char	*value;
+
 	(*s)++;
 	if (**s == '<')
 	{
 		(*s)++;
-		return (new_token(ft_strdup("<<"), TOK_HEREDOC));
+		value = ft_strdup("<<");
+		if (!value)
+			return (NULL);
+		return (safe_new_op(value, TOK_HEREDOC));
 	}
-	return (new_token(ft_strdup("<"), TOK_REDIR_IN));
+	value = ft_strdup("<");
+	if (!value)
+		return (NULL);
+	return (safe_new_op(value, TOK_REDIR_IN));
 }
 
 t_token	*read_operator(char **s)
