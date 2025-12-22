c218cc87a4fb0fd7b0c9f03b62f0aac161ddecbe
kaio lima <kylelima850@hotmail.com>
2025-12-22 20:51:03 -0300

    feat: add free_ast to recursively deallocate the syntax tree

--- src/parser/free_parse.c
+++ src/parser/free_parse.c
@@ -3,36 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   free_parse.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
+/*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 19:47:41 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/12/08 21:10:16 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/12/22 20:40:53 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	free_parse(t_command **cmd)
+void	free_command(t_command *cmd)
 {
-	t_command	*temp;
-	int			i;
+	int	i;
 
-	if (!cmd || !*cmd)
+	if (!cmd)
 		return ;
-	while (*cmd)
+	i = 0;
+	while (cmd->argv && cmd->argv[i])
 	{
-		temp = *cmd;
-		*cmd = (*cmd)->next;
-		i = -1;
-		while (temp->argv && temp->argv[++i])
-			free(temp->argv[i]);
-		free(temp->argv);
-		if (temp->infile)
-			free(temp->infile);
-		if (temp->outfile)
-			free(temp->outfile);
-		if (temp->quoted)
-			free(temp->quoted);
-		free(temp);
+		free(cmd->argv[i]);
+		i++;
 	}
+	free(cmd->argv);
+	if (cmd->infile)
+		free(cmd->infile);
+	if (cmd->outfile)
+		free(cmd->outfile);
+	if (cmd->quoted)
+		free(cmd->quoted);
+	free(cmd);
 }
+
+void	free_ast(t_ast *node)
+{
+	if (!node)
+		return;
+	free_ast(node->left);
+	free_ast(node->right);
+	if (node->token)
+		free_token(node->token);
+	if (node->type == NODE_CMD)
+		free_command(node->cmd);
+	free(node);
+}
+
