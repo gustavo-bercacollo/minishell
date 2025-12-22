bcd826ff3abdede0ebef98a1b7056e45a0a17757
kaio lima <kylelima850@hotmail.com>
2025-12-22 20:49:12 -0300

    refactor: update expansion interface to accept AST nodes

--- src/expansions/expansions.c
+++ src/expansions/expansions.c
@@ -3,36 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   expansions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbercaco <gbercaco@student.42.fr>          +#+  +:+       +#+        */
+/*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 14:46:12 by gbercaco          #+#    #+#             */
-/*   Updated: 2025/12/02 16:05:57 by gbercaco         ###   ########.fr       */
+/*   Updated: 2025/12/22 20:48:37 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	expand(t_shell *ms, t_command *cmd_list)
+/*
+** Expande um ÚNICO comando.
+** Regra: se substituir argv[i], libera o antigo antes.
+*/
+void	expand_cmd(t_shell *ms, t_command *cmd)
 {
-	t_command	*cmd;
-	char		*var_name;
-	int			i;
+	int		i;
+	char	*var_name;
+	char	*new_arg;
 
-	cmd = cmd_list;
-	while (cmd)
+	if (!cmd || !cmd->argv)
+		return ;
+	i = 0;
+	while (cmd->argv[i])
 	{
-		i = 0;
-		while (cmd->argv && cmd->argv[i])
+		if (cmd->quoted && cmd->quoted[i] != 1)
 		{
-			if (cmd->quoted[i] != 1)
+			var_name = process_arg(cmd->argv[i]);
+			if (var_name)
 			{
-				var_name = process_arg(cmd->argv[i]);
-				if (var_name)
-					cmd->argv[i] = expand_variable_in_arg(ms, cmd->argv[i],
-							var_name);
+				new_arg = expand_variable_in_arg(
+						ms, cmd->argv[i], var_name);
+				if (new_arg)
+				{
+					free(cmd->argv[i]);
+					cmd->argv[i] = new_arg;
+				}
 			}
-			i++;
 		}
-		cmd = cmd->next;
+		i++;
+	}
+}
+
+void	expand_ast(t_shell *ms, t_ast *node)
+{
+	if (!node)
+		return ;
+	if (node->type == NODE_CMD)
+		expand_cmd(ms, node->cmd);
+	else
+	{
+		expand_ast(ms, node->left);
+		expand_ast(ms, node->right);
 	}
 }
