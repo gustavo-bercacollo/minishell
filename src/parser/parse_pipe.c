2a6266f2ea900a71dd1d438650fef52ddb508bc0
kaio lima <kylelima850@hotmail.com>
2025-12-22 20:58:16 -0300

    feat: implement parser_pipe to handle command pipelines in AST

--- src/parser/parse_pipe.c
+++ src/parser/parse_pipe.c
@@ -6,29 +6,34 @@
 /*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 15:56:59 by klima-do          #+#    #+#             */
-/*   Updated: 2025/12/18 16:16:15 by klima-do         ###   ########.fr       */
+/*   Updated: 2025/12/22 20:23:33 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_ast 	*parse_pipe(t_token **tok)
+t_ast	*parse_pipe(t_token **tok)
 {
 	t_ast		*left;
 	t_ast		*right;
 	t_command	*cmd;
+	t_token		*pipe_tok;
 
 	cmd = parse_cmd(tok);
-	if(!cmd)
+	if (!cmd)
 		return (NULL);
-
 	left = ast_from_cmd(cmd);
-	while(*tok && (*tok)->type == TOK_PIPE)
+	while (*tok && (*tok)->type == TOK_PIPE)
 	{
+		pipe_tok = *tok;
 		*tok = (*tok)->next;
+		free_token(pipe_tok);
 		cmd = parse_cmd(tok);
 		if (!cmd)
+		{
+			free_ast(left);
 			return (NULL);
+		}
 		right = ast_from_cmd(cmd);
 		left = new_ast_node(NODE_PIPE, left, right);
 	}
