80704983524846eb9d358b696c250164162d89de
kaio lima <kylelima850@hotmail.com>
2025-12-23 01:39:08 -0300

    fix: use _exit in child processes to avoid buffer duplication

--- src/execute/execute_cmd.c
+++ src/execute/execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 19:43:08 by klima-do          #+#    #+#             */
-/*   Updated: 2025/12/22 20:40:16 by klima-do         ###   ########.fr       */
+/*   Updated: 2025/12/22 21:05:50 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ int	execute_cmd(t_shell *ms, t_ast *node)
 	{
 		set_default_signals_for_child();
 		if (apply_redirections(node->cmd) <0)
-			exit (1);
+			_exit (1);
 		path = get_path(ms->envp, node->cmd->argv[0]);
 		if (!path)
 		{
 			ft_putstr_fd(node->cmd->argv[0], 2);
 			ft_putendl_fd(": command not found", 2);
-			exit(127);
+			_exit(127);
 		}
 		execve(path, node->cmd->argv, ms->envp);
 		perror("execve");
