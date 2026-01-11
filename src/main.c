0231e227b4d6a10ff4f4c7c648e374964405ad95
kaio <kylelima850@hotmail.com>
2026-01-11 20:19:32 -0300

    refactor(main): adapt shell loop to new signal handling model

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: klima-do <klima-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 18:45:06 by gbercaco          #+#    #+#             */
-/*   Updated: 2026/01/08 20:24:54 by klima-do         ###   ########.fr       */
+/*   Updated: 2026/01/11 20:15:58 by klima-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ void	init_shell(char **line, t_token **tokens, t_ast **ast, t_shell *ms)
 {
 	while (1)
 	{
-		g_interrupted = 1;
+		set_signals_interactive();
 		*line = readline("minishell> ");
-		g_interrupted = 0;
 		if (!*line)
 			break ;
 		if (**line)
@@ -27,18 +26,19 @@ void	init_shell(char **line, t_token **tokens, t_ast **ast, t_shell *ms)
 		*ast = parse_ast(tokens);
 		expand_ast(ms, *ast);
 		if (*ast)
-    	ms->last_status = execute_node(ms, *ast);
+			ms->last_status = execute_node(ms, *ast);
 		if (ms->should_exit)
 		{
-		    free_tokens(tokens);
-		    free_ast(*ast);
-		    break;
+			free_tokens(tokens);
+			free_ast(*ast);
+			free(*line);
+			break ;
 		}
 		free_tokens(tokens);
 		free_ast(*ast);
+		free(*line);
 		*tokens = NULL;
 		*ast = NULL;
-		free(*line);
 	}
 }
 
@@ -49,14 +49,15 @@ int	main(int argc, char **argv, char **envp)
 	t_token		*tokens;
 	t_ast		*ast;
 
+	(void)argc;
+	(void)argv;
 	line = NULL;
 	tokens = NULL;
 	ast = NULL;
-	(void)argc;
-	(void)argv;
 	ms.env = env_hash_init(envp);
 	ms.last_status = 0;
-	init_signals();
+	ms.should_exit = 0;
+	set_signals_interactive();
 	init_shell(&line, &tokens, &ast, &ms);
 	clear_history();
 	free_hash(ms.env);
